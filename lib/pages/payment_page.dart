9aca9610e1f46d6b338cd019320c5097cf3f6117
jaymesC <clementjaymes@gmail.com>
2022-04-07 22:30:09 -0700

    chore: updated payment_page widget

--- lib/pages/payment_page.dart
+++ lib/pages/payment_page.dart
@@ -46,68 +46,106 @@ class PaymentPage extends StatelessWidget {
               height: h * 0.03,
             ),
             Container(
-              height: 130,
+              height: 140,
               width: 350,
               decoration: BoxDecoration(
                   borderRadius: BorderRadius.circular(10),
                   border: Border.all(
                       width: 2, color: Colors.grey.withOpacity(0.5))),
-              child: Container(
-                  child: Column(
-                children: [
-                  Row(
-                    crossAxisAlignment: CrossAxisAlignment.end,
-                    children: [
-                      Container(
-                        margin: const EdgeInsets.only(top: 10, left: 15),
-                        width: 50,
-                        height: 50,
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(25),
-                          color: Colors.green,
-                        ),
-                        child: Icon(
-                          Icons.done,
-                          size: 30,
-                          color: Colors.white,
-                        ),
-                      ),
-                      SizedBox(
-                        width: 10,
-                      ),
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
+              child: MediaQuery.removePadding(
+                removeTop: true,
+                context: context,
+                child: ListView.builder(
+                  itemCount: 3,
+                  itemBuilder: (_, index) {
+                    return Container(
+                      child: Column(
                         children: [
-                          SizedBox(
-                            height: 5,
+                          Row(
+                            crossAxisAlignment: CrossAxisAlignment.end,
+                            children: [
+                              Container(
+                                margin: const EdgeInsets.only(
+                                  top: 10,
+                                  left: 15,
+                                ),
+                                width: 50,
+                                height: 50,
+                                decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(25),
+                                  color: Colors.green,
+                                ),
+                                child: Icon(
+                                  Icons.done,
+                                  size: 30,
+                                  color: Colors.white,
+                                ),
+                              ),
+                              SizedBox(
+                                width: 10,
+                              ),
+                              Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  SizedBox(
+                                    height: 5,
+                                  ),
+                                  Text('KenGen Power',
+                                      style: TextStyle(
+                                          fontSize: 16,
+                                          color: AppColor.mainColor,
+                                          fontWeight: FontWeight.w700)),
+                                  SizedBox(
+                                    height: 10,
+                                  ),
+                                  Text('ID: 537298',
+                                      style: TextStyle(
+                                          fontSize: 16,
+                                          color: AppColor.idColor,
+                                          fontWeight: FontWeight.w700)),
+                                ],
+                              ),
+                              SizedBox(
+                                width: 20,
+                              ),
+                              Text('\$1248.00',
+                                  style: TextStyle(
+                                      fontSize: 16,
+                                      color: AppColor.mainColor,
+                                      fontWeight: FontWeight.w700)),
+                            ],
                           ),
-                          Text('KenGen Power',
-                              style: TextStyle(
-                                  fontSize: 16,
-                                  color: AppColor.mainColor,
-                                  fontWeight: FontWeight.w700)),
-                          SizedBox(
-                            height: 10,
-                          ),
-                          Text('ID: 537298',
-                              style: TextStyle(
-                                  fontSize: 16,
-                                  color: AppColor.idColor,
-                                  fontWeight: FontWeight.w700)),
+                          Divider(
+                            thickness: 2,
+                            color: Colors.grey.withOpacity(0.5),
+                          )
                         ],
                       ),
-                      SizedBox(
-                        width: 20,
-                      ),
-                      Text('\$1248.00',
-                          style: TextStyle(
-                              fontSize: 16,
-                              color: AppColor.mainColor,
-                              fontWeight: FontWeight.w700)),
-                    ],
-                  )
-                ],
-              )),
+                    );
+                  },
+                ),
+              ),
+            ),
+            SizedBox(
+              height: h * 0.03,
+            ),
+            Column(
+              children: [
+                Text(
+                  'Total Amount',
+                  style: TextStyle(color: AppColor.idColor, fontSize: 20),
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Text(
+                  '\$2840.00',
+                  style: TextStyle(
+                      color: AppColor.mainColor,
+                      fontSize: 30,
+                      fontWeight: FontWeight.w600),
+                ),
+              ],
             )
           ],
         ),
